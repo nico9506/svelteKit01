afd40d370f9093ada9bfd638821d09cbd0d14def
Nicolas Lopez <niclopezt@gmail.com>
2024-08-16 23:01:06 +1000

    feat: optional parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+
+	const greetings = {
+		en: 'Hello!',
+		es: 'Hola!',
+		fr: 'Bonjour!'
+	};
+
+	// asign a default value "en"
+	const { lang = 'en' } = $page.params;
+	const greeting = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
