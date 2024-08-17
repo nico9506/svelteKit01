db2edea9738dd0fcf3db1a1d2b3b88df275f6f0e
Nicolas Lopez <niclopezt@gmail.com>
2024-08-17 22:38:00 +1000

    feat: navigating programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+
+	const handleClick = () => {
+		console.log('Placing your order');
+		goto('/products', { replaceState: true });
+	};
+
+	beforeNavigate((navigation) => {
+		console.log({ before: navigation });
+	});
+	afterNavigate((navigation) => {
+		console.log({ after: navigation });
+	});
+</script>
+
 <h1>Neovim (Lazyvim) + SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
+
+<button on:click={handleClick}>Place Order</button>
