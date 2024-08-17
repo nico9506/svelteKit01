4c4cb78ce1f8d45f140a356f96e93c47d4ed41d4
Nicolas Lopez <niclopezt@gmail.com>
2024-08-17 23:02:42 +1000

    feat: route layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<header class="layout-header">Header</header>
+<slot />
+<footer class="layout-footer">Footer</footer>
+
+<style>
+	.layout-header {
+		background-color: black;
+		color: orange;
+		font-size: 2rem;
+		text-align: center;
+		padding: 1rem;
+	}
+
+	.layout-footer {
+		background-color: black;
+		color: orange;
+		font-size: 2rem;
+		text-align: center;
+		padding: 1rem;
+	}
+</style>
