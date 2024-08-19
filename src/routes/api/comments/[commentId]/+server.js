13511277f8f5a71eee033cf9da06a2c12f729bac
Nicolas Lopez <niclopezt@gmail.com>
2024-08-19 22:28:04 +1000

    feat: add delete endpoint

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -18,3 +18,13 @@ export async function PATCH(requestEvent) {
 
 	return json(comment);
 }
+
+export async function DELETE(requestEvent) {
+	const { params } = requestEvent;
+	const { commentId } = params;
+	const deletedComment = comments.find((comment) => comment.id === parseInt(commentId));
+	const index = comments.findIndex((comment) => comment.id === parseInt(commentId));
+	comments.splice(index, 1);
+
+	return json(deletedComment);
+}
